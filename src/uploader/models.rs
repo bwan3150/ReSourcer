0f7a1b6cfc52337f8d8e1b8f5a44c8904330fc65
Eric Wang <eric.baiyangwang@gmail.com>
2025-10-06 18:49:35 +1100

    Remove: uploader rs files

--- src/uploader/models.rs
+++
@@ -1,9 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-// 设备信息
-#[derive(Debug, Serialize, Deserialize)]
-pub struct DeviceInfo {
-    pub device_id: String,
-    pub device_name: String,
-    pub connected_at: String,
-}
