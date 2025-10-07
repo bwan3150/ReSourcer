4ebbc326c5a72f86d1a033bfb93d1822a1630bcf
Eric Wang <eric.baiyangwang@gmail.com>
2025-10-07 21:05:18 +1100

    Remove: all old single server routes and constants

--- app/flutter/lib/utils/constants.dart
+++ app/flutter/lib/utils/constants.dart
@@ -1,15 +1,5 @@
 /// 常量定义
 class Constants {
-  // 存储键名
-  static const String apiKeyKey = 'api_key';
-  static const String baseUrlKey = 'base_url';
-
-  // 路由名称
-  static const String routeLogin = '/login';
-  static const String routeHome = '/home';
-  static const String routeQrScan = '/qr_scan';
-  static const String routeImageDetail = '/image_detail';
-
   // 页面索引
   static const int tabGallery = 0;
   static const int tabClassifier = 1;
